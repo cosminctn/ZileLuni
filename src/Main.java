c35f201e1dd384dd0b7f29c2b6249d176f943608
cosmincoco <cosmin.constantinescu@ezugi.com>
2019-04-24 00:04:09 +0300

    tema zile luna

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,64 @@
+import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int [] numar_zile = {31, 28, 31, 30,31,30,31,31,30,31,30,31};
+        Scanner user_input = new Scanner(System.in);
+        System.out.println("Introduceti numarul lunii pentru a aflat cate zile are (<12)");
+        int i = user_input.nextInt();
+        switch (i){
+            case 1:
+                System.out.println("Ianuarie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 2:
+                System.out.println("Februarie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 3:
+                System.out.println("Martie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 4:
+                System.out.println("Aprilie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 5:
+                System.out.println("Mai");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 6:
+                System.out.println("Iunie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 7:
+                System.out.println("Iulie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 8:
+                System.out.println("August");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 9:
+                System.out.println("Septembrie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 10:
+                System.out.println("Octombrie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 11:
+                System.out.println("Noiembrie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            case 12:
+                System.out.println("Decembrie");
+                System.out.println(numar_zile[i-1]+ " de zile");
+                break;
+            default:
+                System.out.println("Numar prea mare / Luna incorecta / Format gresit");
+                break;
+        }
+
     }
 }
